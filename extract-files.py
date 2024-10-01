a9e5b416ced0a28ff17a20f4165f30f9b0a78734
Bruno Martins <bgcngm@gmail.com>
2024-10-01 11:41:45 +0100

    FP6: Add libshim_codec2
    Required after commit: https://android.googlesource.com/platform/frameworks/av/+/a9d953ed85b4e494b1d620730a1701eb51d956ed
    
    Change-Id: Ic105b427172eee967e565baa07f10284c0c19724

--- extract-files.py
+++ extract-files.py
@@ -236,7 +236,7 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
-        .add_needed('libcodec2_shim.so'),
+        .add_needed('libshim_codec2.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
 
