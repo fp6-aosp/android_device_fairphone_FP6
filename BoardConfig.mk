f86c2c4a14530a59c432f5edf4867871f0644228
Tim Zimmermann <tim@linux4.de>
2025-10-05 15:20:15 +0200

    FP6: Initial commit
    Change-Id: I531eb563fc2654623fc57be3fed8ed492c58ef42

---
+++ BoardConfig.mk
@@ -0,0 +1,151 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PATH := device/fairphone/FP6
+
+## A/B Updates
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    init_boot \
+    recovery \
+    vendor_boot
+
+## Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo300
+
+## Boot Image
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.load_modules_parallel=true \
+    androidboot.vendor.qspa=true \
+    androidboot.console=0
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    nosoftlockup
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_INIT_BOOT_HEADER_VERSION := 4
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_RAMDISK_USE_LZ4 := true
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
+
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_INIT_ARGS := --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
+
+## Display
+TARGET_SCREEN_DENSITY := xxxhdpi
+
+## DTBO
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+## Dynamic Partitions
+BOARD_SUPER_PARTITION_SIZE := $(shell echo $$(( 9 * 1024 * 1024 * 1024 )))
+
+## Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+## Kernel
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    BRANCH=android14-6.1 \
+    KMI_GENERATION=11 \
+    TARGET_BOARD_PLATFORM=volcano
+TARGET_KERNEL_CONFIG := \
+    gki_defconfig \
+    vendor/fps_GKI.config
+TARGET_KERNEL_NO_GCC := true
+TARGET_KERNEL_SOURCE := kernel/fairphone/sm7635
+
+## Kernel Modules
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/configs/kernel/modules.load.system_dlkm))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/configs/kernel/modules.blocklist
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/configs/kernel/modules.load.vendor_dlkm))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/configs/kernel/modules.load.base))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := \
+    $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD) \
+    $(strip $(shell cat $(DEVICE_PATH)/configs/kernel/modules.load.recovery_extra))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+SYSTEM_KERNEL_MODULES := $(BOARD_SYSTEM_KERNEL_MODULES_LOAD)
+
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/fairphone/sm7635-modules
+TARGET_KERNEL_EXT_MODULES := \
+    fairphone/emkit \
+    fairphone/input/finger \
+    fairphone/input/misc/vl53L1 \
+    fairphone/misc/haptic_hv \
+    qcom/opensource/mmrm-driver \
+    qcom/opensource/mm-drivers/hw_fence \
+    qcom/opensource/mm-drivers/msm_ext_display \
+    qcom/opensource/mm-drivers/sync_fence \
+    qcom/opensource/securemsm-kernel \
+    qcom/opensource/audio-kernel \
+    qcom/opensource/synx-kernel \
+    qcom/opensource/camera-kernel \
+    qcom/opensource/datarmnet-ext/mem \
+    qcom/opensource/dataipa/drivers/platform/msm \
+    qcom/opensource/datarmnet/core \
+    qcom/opensource/datarmnet-ext/aps \
+    qcom/opensource/datarmnet-ext/offload \
+    qcom/opensource/datarmnet-ext/shs \
+    qcom/opensource/datarmnet-ext/perf \
+    qcom/opensource/datarmnet-ext/perf_tether \
+    qcom/opensource/datarmnet-ext/sch \
+    qcom/opensource/datarmnet-ext/wlan \
+    qcom/opensource/display-drivers/msm \
+    qcom/opensource/dsp-kernel \
+    qcom/opensource/eva-kernel \
+    qcom/opensource/video-driver \
+    qcom/opensource/graphics-kernel \
+    qcom/opensource/wlan/platform \
+    qcom/opensource/wlan/qcacld-3.0/.qca6750 \
+    qcom/opensource/wlan/qcacld-3.0/.peach_v2 \
+    qcom/opensource/wlan/qcacld-3.0/.wcn6450 \
+    qcom/opensource/bt-kernel \
+    qcom/opensource/spu-kernel \
+    qcom/opensource/mm-sys-kernel/ubwcp \
+    qcom/opensource/touch-drivers \
+    nxp/opensource/driver \
+    samsung_slsi/nfc/driver
+
+## Partitions
+BOARD_FLASH_BLOCK_SIZE := $(shell echo $$(( $(BOARD_KERNEL_PAGESIZE) * 64 )))
+BOARD_BOOTIMAGE_PARTITION_SIZE := $(shell echo $$(( 96 * 1024 * 1024 )))
+BOARD_DTBOIMG_PARTITION_SIZE := $(shell echo $$(( 30 * 1024 * 1024 )))
+BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE := $(shell echo $$(( 8 * 1024 * 1024 )))
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := $(shell echo $$(( 100 * 1024 * 1024 )))
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := $(shell echo $$(( 96 * 1024 * 1024 )))
+
+BOARD_USES_METADATA_PARTITION := true
+
+## Platform
+TARGET_BOARD_PLATFORM := volcano
+TARGET_BOOTLOADER_BOARD_NAME := fps
+
+## Recovery
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/configs/init/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
---
+++ device.mk
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/fairphone/FP6
+
+# Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Enable virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+$(call inherit-product, \
+    $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+
+PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# A/B support
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+# Device identifier, this must come after all inclusions
+PRODUCT_NAME := FP6
+PRODUCT_NAME_FOR_ATTESTATION := FP6
+PRODUCT_DEVICE := FP6
+PRODUCT_BRAND := Fairphone
+PRODUCT_BRAND_FOR_ATTESTATION := Fairphone
+PRODUCT_MODEL := Fairphone 6
+PRODUCT_MODEL_FOR_ATTESTATION := Fairphone 6
+PRODUCT_MANUFACTURER := Fairphone
+PRODUCT_SHIPPING_API_LEVEL := 35
+BOARD_SHIPPING_API_LEVEL := 34
+
+PRODUCT_GMS_CLIENTID_BASE := android-fairphone
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# init
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
+    $(DEVICE_PATH)/configs/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(DEVICE_PATH)/configs/init/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
+
+# Soong Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
