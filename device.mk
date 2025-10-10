07ae795aa514198060a220f32f465e89819dde85
Tim Zimmermann <tim@linux4.de>
2025-10-10 06:50:52 +0200

    FP6: Add A/B postinstall config
    Change-Id: I030c40cc6a2af69116f5af3d74267ccd9e587ba0

--- device.mk
+++ device.mk
@@ -36,9 +36,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # A/B support
 PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script \
     update_engine \
     update_engine_sideload \
     update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
 
 # Device identifier, this must come after all inclusions
 PRODUCT_NAME := FP6
