f0fb2d179950de83fcf2aec1c34befab28a24654
Tim Zimmermann <tim@linux4.de>
2025-10-13 18:54:38 +0200

    FP6: Refactor overlays
    * Merge Common and Target overlays and rename to FP6*ResTarget
    
    Change-Id: I29f3681dec5021a7d4aa4afbcc9e06a97159ad7c

--- device.mk
+++ device.mk
@@ -247,17 +247,15 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    CellBroadcastReceiverResCommon_Vendor \
-    FrameworksResCommon_Vendor \
-    FrameworksResTarget_Vendor \
-    SecureElementResTarget_Vendor \
-    SettingsResCommon_Vendor \
-    SystemUIResCommon_Vendor \
-    TelecommResCommon_Vendor \
-    TelephonyResCommon_Vendor \
-    UwbResCommon_Vendor \
-    WifiResCommon_Vendor \
-    WifiResTarget
+    FP6CellBroadcastReceiverResTarget \
+    FP6FrameworksResTarget \
+    FP6SecureElementResTarget \
+    FP6SettingsResTarget \
+    FP6SystemUIResTarget \
+    FP6TelecommResTarget \
+    FP6TelephonyResTarget \
+    FP6UwbResTarget \
+    FP6WifiResTarget
 
 # Permissions
 PRODUCT_PACKAGES += \
