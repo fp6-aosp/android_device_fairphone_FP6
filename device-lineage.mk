1201c1c97fc17bfb3dc717edd34406add04860a2
Tim Zimmermann <tim@linux4.de>
2025-10-24 08:52:21 +0200

    FP6: Add support for toggling fast charge
    Change-Id: I1a6d8b980beede8036c61456f8199f39c67c2c99

--- device-lineage.mk
+++ device-lineage.mk
@@ -20,6 +20,9 @@ DEVICE_PATH := device/fairphone/FP6
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/charge_disable)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/chgmod_fcc)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1000000)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,5800000)
 
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
