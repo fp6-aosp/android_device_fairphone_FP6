4083939ea18095805ee58e0767c119521190bf01
Tim Zimmermann <tim@linux4.de>
2026-01-06 06:30:10 +0100

    FP6: Disable pstore compression
    * This allows to find pstore in an EDL memory dump
    
    Change-Id: I1411e189f1861ce3a714b283fad5fc4bf8183b7d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,8 @@ BOARD_BOOTCONFIG := \
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
-    nosoftlockup
+    nosoftlockup \
+    pstore.compress=none
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_INIT_BOOT_HEADER_VERSION := 4
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
