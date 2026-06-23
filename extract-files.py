53436b4e2306c016949044172f9738dc5dbcc629
Arian <arian.kulmer@web.de>
2026-06-23 12:36:19 +0200

    FP6: Silence perf spam on boot
    The perf stack waits for boot to be completed to start operating and
    for some reason QCOM thought to print an error every few ms until
    vendor.post_boot.parsed is set to 1. This spams during the whole boot
    process. Replace the log print call with nop:
    @@ -1041,7 +1041,7 @@
         3fe8: 528000c0     	mov	w0, #0x6                // =6
         3fec: aa0103e3     	mov	x3, x1
         3ff0: 52800c05     	mov	w5, #0x60               // =96
    -    3ff4: 94000887     	bl	0x6210 <__android_log_print@plt>
    +    3ff4: d503201f     	nop
         3ff8: 12800054     	mov	w20, #-0x3              // =-3
         3ffc: 140000e7     	b	0x4398 <_ZN13PerfGlueLayer26PerfGlueLayerSubmitRequestEP11mpctl_msg_t+0x418>
         4000: b4000453     	cbz	x19, 0x4088 <_ZN13PerfGlueLayer26PerfGlueLayerSubmitRequestEP11mpctl_msg_t+0x108>
    
    Removes this log entry:
    E ANDR-PERF-GLUELAYER: ANDR-PERF-GLUELAYER: PerfGlueLayerSubmitRequest() 96: Request not Submitted as Target Init is not Complete
    
    Change-Id: I8bf81141f1ff3c0b5fa3002eed9591c31cb2bf61

--- extract-files.py
+++ extract-files.py
@@ -234,6 +234,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/libperfgluelayer.so': blob_fixup()
+        .sig_replace('87 08 00 94', '1F 20 03 D5'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libshim_codec2.so'),
     'vendor/lib64/libril-db.so': blob_fixup()
