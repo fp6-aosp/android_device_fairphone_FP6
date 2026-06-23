f9fc1c76704d51524cd2950a069a3496ccab41ef
Arian <arian.kulmer@web.de>
2026-06-23 12:46:48 +0200

    FP6: Disable all CLO-specific perf features
    This tried to enable plenty of features which we do not support on
    AOSP, like silky scroll and learning module. It also set some
    properties for CLO additions to lmk, BoostFramework and other changes.
    None of these are relevant for us on AOSP.
    
    The perf stack skips the regular interaction boost when silky scroll is
    enabled, so disable all the CLO features to get the regular interaction
    and launch boosts working when sent via the qcom power aidl.
    
    The new perfconfigstore.xml is constructed from the properties read by
    the HAL:
    * Remove perfconfigstore.xml
    * setprop vendor.debug.trace.perf.level 1
    * stop perf2-hal-1-0 && start perf2-hal-1-0
    * Observe which properties it tries to get in logcat
    
    Change-Id: I0bac510ae71ee0062b81ad499fafc53bef6de6ce

--- device.mk
+++ device.mk
@@ -333,6 +333,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
+
 # Power
 $(call soong_config_set,qtipower,tap_to_wake_node,/sys/bus/spi/devices/spi0.0/gesture_wakeup)
 
