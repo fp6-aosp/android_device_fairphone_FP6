4dd7c0abbd33756a8198c0ef23cdd2714ced3fb0
Arian <arian.kulmer@web.de>
2026-07-24 20:51:38 +0200

    FP6: Pin wifi firmware from FP6.QREL.15.176.0
    There seem to be issues with IPv6 since the upgrade to Android 16.
    Extract the relevant wifi firmware files from the Android 15
    NON-HLOS.bin and copy them to vendor/firmware which takes priority over
    vendor/firmware_mnt where modem is mounted.
    
    More details on the issue can be found here:
    https://forum.fairphone.com/t/dns-over-tls-ipv6-issues-apps-dont-load-data-over-wifi/130519
    
    Change-Id: I45c3144c9f03d9af1edc612bda152724cb5afe6c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,8 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     nosoftlockup \
     pstore.compress=none \
-    sysctl.kernel.firmware_config.force_sysfs_fallback=1
+    sysctl.kernel.firmware_config.force_sysfs_fallback=1 \
+    firmware_class.path=/vendor/firmware,/vendor/firmware_mnt/image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_INIT_BOOT_HEADER_VERSION := 4
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
